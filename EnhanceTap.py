b1dca6bb661cb70df65ef213a179de5b58a8c594
choihj00 <add927@kyonggi.ac.kr>
2022-05-18 02:43:47 +0900

    [최희정] histogram equalization 기능 구현

---
+++ EnhanceTap.py
@@ -0,0 +1,22 @@
+class EnhanceTap():
+    def __init__(self):
+        pass
+
+    def histogram_equalization(self):
+        from PIL import Image
+        from skimage import io, exposure
+        from matplotlib import pyplot as plt
+        from skimage.io import imread
+
+        im = io.imread(self.image_source)
+
+        im_equalization = exposure.equalize_hist(im)
+
+        plt.figure(figsize=(4, 5))
+        plt.axis('off')
+        plt.imshow(im_equalization)
+        plt.savefig('Histogram_Equalization.png')
+
+        img2 = imread('Histogram_Equalization.png')
+        self.show_image(self.lblImage2_E, img2)
+
