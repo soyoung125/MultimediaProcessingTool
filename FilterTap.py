09a05e1972176dc94c57be8beaf70e6f4ade35fa
choihj00 <add927@kyonggi.ac.kr>
2022-05-18 19:56:10 +0900

    [최희정] smoothing 기능 구현

--- FilterTap.py
+++ FilterTap.py
@@ -8,9 +8,9 @@ class FilterTap():
         from skimage import filters, io
         from skimage.io import imread
 
-        im = io.imread(self.image_source)
+        img = io.imread(self.image_source)
         sigma = int(self.txtSigma.toPlainText())
-        im_blur = filters.gaussian(im, sigma=sigma, channel_axis=-1)
+        im_blur = filters.gaussian(img, sigma=sigma, channel_axis=-1)
         plt.figure(figsize=(4, 5))
         plt.axis('off')
         plt.imshow(im_blur)
@@ -19,8 +19,28 @@ class FilterTap():
         img2 = imread('BlurImage.png')
         self.show_image(self.lblImage2_F, img2)
 
-    #def show_smooth(self):
+    def show_smooth(self):
+        import cv2
+        import numpy as np
+        import matplotlib.pyplot as plt
+        from skimage import filters, io
+        from skimage.io import imread
 
+        img = io.imread(self.image_source)
 
-    #def show_sharp(self):
+        smoothing_mask = np.array([[1 / 16, 1 / 8, 1 / 16], [1 / 8, 1 / 4, 1 / 8], [1 / 16, 1 / 8, 1 / 16]])
+        im_smooth = cv2.filter2D(img, -1, smoothing_mask)
 
+        plt.figure(figsize=(4, 5))
+        plt.axis('off')
+        plt.imshow(im_smooth)
+        plt.savefig('SmoothImage.png')
+
+        img2 = imread('SmoothImage.png')
+
+        self.show_image(self.lblImage2_F, img2)
+
+        # pip uninstall opencv-contrib-python
+        # pip install opencv-contrib-python    cv2 오류 발생시 설치 필요
+
+    # def show_sharp(self):
