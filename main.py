2810dc1a4404c9f3c8d513c695219a370563dd2e
choihj00 <add927@kyonggi.ac.kr>
2022-05-20 05:09:30 +0900

    open video 구현

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class image_processing_class(QMainWindow):
         loadUi('image_processing_app.ui', self)
         self.image_source = None
         self.source_image = None
+        self.source_video = None
 
         # Tab improvement
         self.bntImpOpenImage_2.clicked.connect(lambda: self.open_image2())
@@ -34,6 +35,9 @@ class image_processing_class(QMainWindow):
         self.btnBlur.clicked.connect(lambda: FilterTap.show_blur(self))
         self.btnSmoothing.clicked.connect(lambda: FilterTap.show_smooth(self))
         self.btnSharpening.clicked.connect(lambda: FilterTap.show_sharp(self))
+
+        # Tap VideoTransformation
+        self.btnOpenVideo.clicked.connect(lambda: self.open_video())
 
     def open_image(self):
         from PyQt5 import QtWidgets, QtCore
@@ -79,6 +83,17 @@ class image_processing_class(QMainWindow):
         except Exception as e:
             print('Error: {'.format(e))
 
+    def open_image5(self):
+        from PyQt5 import QtWidgets, QtCore
+        from skimage import io
+        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File', QtCore.QDir.rootPath(),'*.*')
+        try:
+            self.image_source = fileName
+            self.source_image = io.imread(fileName)
+            self.show_image(self.lblVideo1, self.source_image)
+        except Exception as e:
+            print('Error: {'.format(e))
+
     def show_image(self, label, image):
         import qimage2ndarray
         from PyQt5 import QtGui
@@ -89,6 +104,27 @@ class image_processing_class(QMainWindow):
         except Exception as e:
             print('Error: {}'.format(e))
 
+    def open_video(self):
+        import cv2
+        from PyQt5 import QtWidgets, QtCore
+        from skimage import io
+
+        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File', QtCore.QDir.rootPath(), '*.*')
+        cap = cv2.VideoCapture(fileName)
+
+        while True:
+            ret, self.image_source = cap.read()
+            self.image_source = cv2.cvtColor(self.image_source, cv2.COLOR_BGR2RGB)
+            self.show_image(self.lblVideo1, self.image_source)
+            # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+            # w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+            # h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+            # print("변환된 동영상 너비(가로) : {}, 높이(세로) : {}".format(w, h))
+            cv2.waitKey(24)
+        cap.release()
+        cv2.destroyAllWindows()
+
+    # def show_video(self):
 
 def image_processing_app():
     import sys
