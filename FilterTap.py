0a570ef33e61afe7333705cd91bfe4b9dae92e85
choihj00 <add927@kyonggi.ac.kr>
2022-05-19 11:13:37 +0900

    Sharpening 기능 구현

--- FilterTap.py
+++ FilterTap.py
@@ -3,7 +3,6 @@ class FilterTap():
         pass
 
     def show_blur(self):
-        from PIL import Image
         import matplotlib.pyplot as plt
         from skimage import filters, io
         from skimage.io import imread
@@ -43,4 +42,55 @@ class FilterTap():
         # pip uninstall opencv-contrib-python
         # pip install opencv-contrib-python    cv2 오류 발생시 설치 필요
 
-    # def show_sharp(self):
+    def show_sharp(self):
+        import cv2
+        import numpy as np
+        import matplotlib.pyplot as plt
+        from skimage import io
+        from skimage.io import imread
+        from PIL import Image
+
+        img = io.imread(self.image_source)
+
+        direction = self.cbbSharp.currentText()
+
+        if direction == '1':
+            sharpening_mask = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
+        elif direction == '3':
+            sharpening_mask = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+        elif direction == '5':
+            sharpening_mask = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+        elif direction == '7':
+            sharpening_mask = np.array([[-1, -1, 0], [-1, 7, -1], [0, -1, -1]])
+        elif direction == '9':
+            sharpening_mask = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+
+        # sharpening_mask = np.array([[0 for j in range(3)] for i in range(3)])
+        # nsum = 0
+        # sigma_sharp = int(self.txtSharp.toPlainText())
+        # sharpening_mask[1][1] = sigma_sharp
+        #
+        # for i in range(0, 2):
+        #     for j in range(0, 2):
+        #         nsum = nsum + sharpening_mask[i][j]
+        #
+        # while nsum != 1:
+        #     sharpening_mask[1][0] = sharpening_mask[1][0] - 1
+        #     sharpening_mask[1][2] = sharpening_mask[1][2] - 1
+        #     sharpening_mask[0][1] = sharpening_mask[0][1] - 1
+        #     sharpening_mask[2][1] = sharpening_mask[2][1] - 1
+        #     sharpening_mask[0][0] = sharpening_mask[0][0] - 1
+        #     sharpening_mask[0][2] = sharpening_mask[0][2] - 1
+        #     sharpening_mask[2][0] = sharpening_mask[2][0] - 1
+        #     sharpening_mask[2][2] = sharpening_mask[2][2] - 1
+
+        im_sharp = cv2.filter2D(img, -1, sharpening_mask)
+
+        plt.figure(figsize=(4, 5))
+        plt.axis('off')
+        plt.imshow(im_sharp)
+        plt.savefig('SharpImage.png')
+
+        img2 = imread('SharpImage.png')
+
+        self.show_image(self.lblImage2_F, img2)
