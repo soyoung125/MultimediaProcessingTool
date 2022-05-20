a9776181f0e7aaff114980637cac2392c9160a86
choihj00 <add927@kyonggi.ac.kr>
2022-05-20 19:19:58 +0900

    video_flip 구현

--- VideoTransformationTap.py
+++ VideoTransformationTap.py
@@ -3,26 +3,7 @@ class VideoTransformationTap:
         pass
 
     def video_filp(self):
-        # import sys
-        # import numpy as np
-        # import cv2
-        # import matplotlib.pyplot as plt
-        # from skimage import filters, io
-        # from skimage.io import imread
-        #
-        # video = cv2.VideoCapture(self.video_source)
-        #
-        # while True:
-        #     ret, video1 = video.read()
-        #     video1 = cv2.cvtColor(video1, cv2.COLOR_BGR2RGB)
-        #     video2 = (video1.shape[1] / 2, video1.shape[0] / 2)
-        #     rot = cv2.getRotationMatrix2D(video2, 90, 0.25)
-        #     video_rotate = cv2.warpAffine(video2, rot, (0, 0))
-        #     self.show_image(self.lblVideo2, video_rotate)
-        #
-        #     cv2.waitKey(24)
         import cv2
-        from PyQt5 import QtWidgets, QtCore
 
         video = cv2.VideoCapture(self.video_source)
 
@@ -38,7 +19,7 @@ class VideoTransformationTap:
         while True:
             ret, video1 = video.read()
             video1 = cv2.cvtColor(video1, cv2.COLOR_BGR2RGB)
-            resize_video = cv2.resize(video1, (320, 180), interpolation=cv2.INTER_CUBIC)
+            resize_video = cv2.resize(video1, (180, 320), interpolation=cv2.INTER_CUBIC)
             rows, cols = resize_video.shape[:2]
             rot = cv2.getRotationMatrix2D((cols/2, rows/2), angle, 1)
             video_rotate = cv2.warpAffine(resize_video, rot, (0, 0))
