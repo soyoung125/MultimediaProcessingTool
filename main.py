e8f712c9b5d6311f8b64b80838fba6ecaecd0c0e
choihj00 <add927@kyonggi.ac.kr>
2022-05-20 09:46:21 +0900

    open_video 동영상 오픈시 크기 조절

--- main.py
+++ main.py
@@ -83,17 +83,6 @@ class image_processing_class(QMainWindow):
         except Exception as e:
             print('Error: {'.format(e))
 
-    def open_image5(self):
-        from PyQt5 import QtWidgets, QtCore
-        from skimage import io
-        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File', QtCore.QDir.rootPath(),'*.*')
-        try:
-            self.image_source = fileName
-            self.source_image = io.imread(fileName)
-            self.show_image(self.lblVideo1, self.source_image)
-        except Exception as e:
-            print('Error: {'.format(e))
-
     def show_image(self, label, image):
         import qimage2ndarray
         from PyQt5 import QtGui
