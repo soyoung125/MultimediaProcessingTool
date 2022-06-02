7185ad76733e9545637a3e992a6ff3b495f76e71
choihj00 <add927@kyonggi.ac.kr>
2022-06-02 11:14:36 +0900

    글자색 변경

--- Tab/CountPersonTap.py
+++ Tab/CountPersonTap.py
@@ -2,16 +2,10 @@ class CountPersonTap:
     def __init__(self):
         pass
 
-    # Counting People
     def count_people(self):
-        import numpy as np
         import cv2
-        from matplotlib import pyplot as plt
 
         cap = cv2.VideoCapture(0)
-
-        # hog = cv2.HOGDescriptor()
-        # hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
         face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
@@ -23,7 +17,6 @@ class CountPersonTap:
 
             faces = face_cascade.detectMultiScale(frame, 1.3, 5)
 
-            # 검출 결과 화면 표시
             person = 1
 
             for (x, y, w, h) in faces:
@@ -31,7 +24,7 @@ class CountPersonTap:
                 cv2.putText(frame, f'person {person}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
                 person += 1
 
-            cv2.putText(frame, f'Total Persons : {person - 1}', (40, 70), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0),
+            cv2.putText(frame, f'Total People : {person - 1}', (40, 70), cv2.FONT_HERSHEY_DUPLEX, 0.8, (0, 0, 0),
                         2)
             self.show_image(self.lblcount, frame)
 
